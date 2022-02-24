61c92e547138d256533349fefca38c9db0c9f98c
soy001 <97643006+soy001@users.noreply.github.com>
2022-02-24 12:40:51 -0800

    Added lab report test cases

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -33,4 +33,28 @@ public class MarkdownParseTest {
             assertEquals("Testing correct output for " + file.getFileName(), expectedResults[i], output);
         }
     }
+
+    @Test
+    public void testSnippet1() throws IOException{
+        String path = "snippet1.md";
+        String contents= Files.readString(Path.of(path));
+        List<String> expect = List.of("google.com", "google.com", "ucsd.edu");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
+
+    @Test 
+    public void testSnippet2() throws IOException{
+        String path = "snippet2.md";
+        String contents= Files.readString(Path.of(path));
+        List<String> expect = List.of("a.com", "a.com", "example.com");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
+
+    @Test 
+    public void testSnippet3() throws IOException{
+        String path = "snippet3.md";
+        String contents= Files.readString(Path.of(path));
+        List<String> expect = List.of("https://www.twitter.com", "https://ucsd-cse15l-w22.github.io/", "https://cse.ucsd.edu/");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
 }
